8fe11e2372115b4ab231f2864793b3b28108a06e
VinK69 <vincentroi2@gmail.com>
2024-03-03 14:29:24 -0500

    Add BaseViewModel

--- MvvmDiMauiLogin/ViewModels/HomePageViewModel.cs
+++ MvvmDiMauiLogin/ViewModels/HomePageViewModel.cs
@@ -1,24 +1,6 @@
-﻿using System.ComponentModel;
-using System.Runtime.CompilerServices;
-
-namespace MvvmDiMauiLogin.ViewModels
+﻿namespace MvvmDiMauiLogin.ViewModels
 { 
-  public class HomePageViewModel :INotifyPropertyChanged
-  {
-    
-    public event PropertyChangedEventHandler PropertyChanged;
-
-    protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+  public class HomePageViewModel : BaseViewModel
     {
-      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
-    {
-      if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-      field = value;
-      RaisePropertyChanged(propertyName);
-      return true;
-    }
   }
 }
